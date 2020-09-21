c7670c6fedaccaa99a265d971da8499089c2c55c
Stuti <stutibansal99@gmail.com>
2020-09-21 17:33:00 +0530

    making a model class so as to store the .csv file data

---
+++ src/main/java/com/labproject/covidtracker/models/LocationInfo.java
@@ -0,0 +1,44 @@
+package com.labproject.covidtracker.models;
+/*
+we are saving the data which we get when we fetch the info from the url and then read the .csv file. So that we can use
+it in future use
+ */
+public class LocationInfo {
+
+    private String state;
+    private String country;
+    private int latestCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestCases() {
+        return latestCases;
+    }
+
+    public void setLatestCases(int latestCases) {
+        this.latestCases = latestCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationInfo{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestCases=" + latestCases +
+                '}';
+    }
+}
